7b06f37980770c64b93575a2dd13fc3522741af4
Calciferr-sudo <shettyvikas608@gmail.com>
2025-05-28 16:13:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ function checkBingo() {
   if ([0, 6, 12, 18, 24].every(i => isMarked(i))) bingoLines++;
   if ([4, 8, 12, 16, 20].every(i => isMarked(i))) bingoLines++;
 
-  return bingoLines >= 5;
+  return bingoLines >= 1;
 }
 
 // Disable all cells (after game over)
